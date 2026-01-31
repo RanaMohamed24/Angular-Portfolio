905528d5ef47a857bc6dd7564a293298bd921d84
RanaMohamed24 <raanaamohamed695@gmail.com>
2026-01-31 13:55:00 +0200

    Initial commit: Angular Portfolio project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Hello Angular 👋</h1>
+    <p>My first Angular project</p>
+  `
+})
+export class AppComponent {}
