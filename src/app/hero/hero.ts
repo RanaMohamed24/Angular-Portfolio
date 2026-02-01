37f70ed4bf7860ce9df4c4a53cc73c44a59abcb3
RanaMohamed24 <raanaamohamed695@gmail.com>
2026-02-01 07:46:13 +0200

    hero component

---
+++ src/app/hero/hero.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  imports: [],
+  templateUrl: './hero.html',
+  styleUrl: './hero.css',
+})
+export class Hero {
+   name = 'Rana Mohamed';
+  title = 'A Frontend Developer & Computer Science graduate passionate about creating intuitive, scalable, and visually stunning digital experiences.';
+
+}
